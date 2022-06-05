54c435255eaff346eb306160cd919e0640662df7
mohdshorab <mohdshorab474@gmail.com>
2022-06-05 19:50:17 +0530

    FluttersharedPreferences/

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/logIn_screen.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    )
+  );
+}
---
+++ lib/userInfo.dart
@@ -0,0 +1,7 @@
+class UserInfo{
+  final String uname;
+  final String pwd;
+  final bool remember;
+
+  UserInfo(this.uname, this.pwd, this.remember);
+}
